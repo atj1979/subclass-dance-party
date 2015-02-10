658c3cb0d48ba366c9b44965739e1ad742dcc989
atj1979 <atj1979@gmail.com>
2015-02-10 12:35:58 -0800

    Add background and lineup functionality
    -Add a horizontal lineup function
    -Add background - needs a resize refactor

--- src/init.js
+++ src/init.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
   window.dancers = [];
+  backgroundArr =[
+      'http://pichost.me/1770974/',
+      'http://designdawgs.net/wp-content/uploads/2014/08/Zebra-Dance-Floor-3.jpg',
+      'http://designdawgs.net/wp-content/uploads/2014/08/Zebra-Dance-Floor-3.jpg',
+      'http://www.hollywoodballroomdc.com/wp-content/uploads/2012/02/ballrom-windows-empty-A.jpg'
+
+
+
+  ];
+  var background = Dancer.prototype.generateImage(backgroundArr);
+  console.log(background);
+  $('body').css({background : 'url('+background+')'});
+
+  //find a close pair
+  //dancer1pos = $(window.dancers[i]).position()
+  //dancer1pos.left
+  //dancer1.top
+  //dancer2....
+  //
+  //Math.hypot(dancer1pos.left-dancer2pos.left, dancer1pos.top-dancer2pos.top);
+  //
+  $(".lineup").on("click", function (event){
+
+    for (var i = 0; i < window.dancers.length; i++){
+        var left = $(document).width() / window.dancers.length;
+        left = left * i;
+        $(window.dancers[i]).animate({top: '500px', left : left+'px'});
+    }
+  });
+
 
   $(".addDancerButton").on("click", function(event){
     /* This function sets up the click handlers for the create-dancer
@@ -17,6 +47,7 @@ $(document).ready(function(){
      */
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
 
+
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
@@ -27,6 +58,7 @@ $(document).ready(function(){
       $("body").width() * Math.random(),
       Math.random() * 5000
     );
+    window.dancers.push(dancer.$node);
     // pseudo-classical"
     // var blinkyDancer = new BlinkyDancer(
     //   $("body").height() * Math.random(),
