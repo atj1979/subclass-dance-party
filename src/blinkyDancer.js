219a438e464a52dbad75675b2828193cd07892af
atj1979 <atj1979@gmail.com>
2015-02-09 17:30:42 -0800

    Fix blinkiness
    - blinkyDancer.js -> Change slideToggle() to toggle()

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -3,7 +3,6 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  //this.oldStep = Dancer.prototype.step;
 
 };
 
@@ -20,5 +19,5 @@ BlinkyDancer.prototype.step = function(){
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.slideToggle();
+  this.$node.toggle();
 };
